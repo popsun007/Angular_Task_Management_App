e7ac289a7d6b1fc7908ed5f0970e9230e507ec05
Frank Sun <popsun2010@gmail.com>
2015-09-25 10:53:15 -0700

    Fix typo for client folder

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 
 var app = express();
-app.use(express.static(path.join(__dirname, "./clint")));
+app.use(express.static(path.join(__dirname, "./client")));
 app.use(bodyParser.json());
 
 app.listen(8000, function()
